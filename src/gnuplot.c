23fe1ed6ac7f11473a111b552cd8aacec29bad13
Clement_T <root@Pericls.localdomain>
2018-11-14 16:02:02 +0100

    Changements divers

--- src/gnuplot.c
+++ src/gnuplot.c
@@ -5,6 +5,9 @@
 	Cette librairie appelle le fichier lorentz.sh qui lance gnuplot avec les bons parametres.
 */
   
-void trace_courbe(){
+void trace_courbe(char * FICHIER){
+	
+	w_log(FICHIER, "Ouverture de Gnuplot.");
+	
 	system("./lorentz.sh");
 }
