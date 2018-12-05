cc613b8fcfeba0c0d8377b59242ebc8a746ef4d6
Clement Truillet <clem.truillet@gmail.com>
2018-12-05 13:11:41 +0100

    Commentaires sur les fichiers .sh

--- bin/lorenz.sh
+++ bin/lorenz.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+#Permet de lancer un serveur X
 export DISPLAY=:0
 
-gnuplot ../data/gnuplot_param/lorenz_param
+#On lance Gnuplot
+gnuplot ../data/gnuplot_param/lorenz_param 
 
--- bin/rossler.sh
+++ bin/rossler.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+#Permet de lancer un serveur X
 export DISPLAY=:0
 
+#On lance Gnuplot
 gnuplot ../data/gnuplot_param/rossler_param
 
